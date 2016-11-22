38dae2fcf0f4b40e4e70b0160f46fad49f749102
maddiewu <mcw2175@barnard.edu>
2016-11-22 15:41:16 -0500

    Create Fibonacci.java
    Implementation of the Fibonacci sequence using a HashMap for memoization.

---
+++ hashing/Fibonacci.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+public class Fibonacci {
+	int callsToFib;
+	int result;
+
+	HashMap<Integer, Integer> hashMem = new HashMap<>();
+
+	public Fibonacci(int n){
+		this.callsToFib = 0;
+		this.result = fib(n);
+	}
+	
+	private int fib(int n) {
+		callsToFib++;
+		if (n == 0) {
+			return 0;
+		} else if (n == 1) {
+			return 1;
+		} else {
+			if (! hashMem.containsKey(n)) {
+				hashMem.put(n, fib(n - 1) + fib(n - 2));
+			}
+			return hashMem.get(n);
+			/*int returnValue = fib(n - 1) + fib(n - 2);
+			return returnValue;*/
+		}
+	}
+
+}
